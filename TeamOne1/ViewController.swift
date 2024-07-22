e6d8728ecfc9dcbe05dfb59834b30b713b841b20
bloom <kimrinoffdev@gmail.com>
2024-07-22 11:24:18 +0900

    Initial Commit

---
+++ TeamOne1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TeamOne1
+//
+//  Created by bloom on 7/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
