0618c973e849270d03fbb048d84d8cd8e4c09240
bloom <kimrinoffdev@gmail.com>
2024-07-22 17:04:16 +0900

    middle commit

--- TeamOne1/ViewController.swift
+++ TeamOne1/ViewController.swift
@@ -6,12 +6,19 @@
 //
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
+  let movieview = MovieDetailView()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
+    view.addSubview(movieview)
+    movieview.snp.makeConstraints{
+      $0.edges.equalToSuperview()
+    }
+    
+    
     // Do any additional setup after loading the view.
   }
 
