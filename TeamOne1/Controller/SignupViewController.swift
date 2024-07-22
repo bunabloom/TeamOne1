8faedf03bba3876f5a8447564c2ab0b474f2ec1a
bloom <kimrinoffdev@gmail.com>
2024-07-23 04:11:26 +0900

    seperate View/ ViewController

--- TeamOne1/Controller/SignupViewController.swift
+++ TeamOne1/Controller/SignupViewController.swift
@@ -6,133 +6,35 @@
 //
 
 import UIKit
+import SnapKit
 
-class SignupViewController: UIViewController {
+class SignupViewController: UIViewController, ConstraintRelatableTarget {
+  let signUpView = SignUPView()
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    configureBasic()
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.text = "회원 가입"
-        label.font = UIFont.boldSystemFont(ofSize: 24)
-        label.textAlignment = .center
-        return label
-    }()
     
-    private let usernameTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "이름"
-        textField.borderStyle = .roundedRect
-        return textField
-    }()
     
-    private let userbirthTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "생년월일"
-        textField.borderStyle = .roundedRect
-        return textField
-    }()
-    
-    private let useridTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "ID"
-        textField.borderStyle = .roundedRect
-        return textField
-    }()
-
-    private let passwordTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "PW"
-        textField.borderStyle = .roundedRect
-        textField.isSecureTextEntry = true
-        return textField
-    }()
-
-    
-
-    private let signupButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Sign Up", for: .normal)
-        button.backgroundColor = .systemBlue
-        button.setTitleColor(.white, for: .normal)
-        button.layer.cornerRadius = 5
-        button.addTarget(self, action: #selector(signupTapped), for: .touchUpInside)
-        return button
-    }()
-
-    private let cancelButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Cancel", for: .normal)
-        button.addTarget(self, action: #selector(cancelTapped), for: .touchUpInside)
-        return button
-    }()
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-
-        view.addSubview(titleLabel)
-        view.addSubview(usernameTextField)
-        view.addSubview(userbirthTextField)
-        view.addSubview(useridTextField)
-        view.addSubview(passwordTextField)
-        view.addSubview(signupButton)
-        view.addSubview(cancelButton)
-
-        setupLayout()
+  }
+  func configureBasic(){
+    view.addSubview(signUpView)
+    signUpView.snp.makeConstraints{
+      $0.edges.equalToSuperview()
     }
-
-    private func setupLayout() {
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
-        userbirthTextField.translatesAutoresizingMaskIntoConstraints = false
-        useridTextField.translatesAutoresizingMaskIntoConstraints = false
-        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
-        signupButton.translatesAutoresizingMaskIntoConstraints = false
-        cancelButton.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
-            titleLabel.widthAnchor.constraint(equalToConstant: 300),
-            titleLabel.heightAnchor.constraint(equalToConstant: 40),
-            
-            usernameTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            usernameTextField.centerYAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 50),
-            usernameTextField.widthAnchor.constraint(equalToConstant: 300),
-            usernameTextField.heightAnchor.constraint(equalToConstant: 40),
-            
-            userbirthTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            userbirthTextField.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 20),
-            userbirthTextField.widthAnchor.constraint(equalToConstant: 300),
-            userbirthTextField.heightAnchor.constraint(equalToConstant: 40),
-            
-            useridTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            useridTextField.topAnchor.constraint(equalTo: userbirthTextField.bottomAnchor, constant: 20),
-            useridTextField.widthAnchor.constraint(equalToConstant: 300),
-            useridTextField.heightAnchor.constraint(equalToConstant: 40),
-
-            passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            passwordTextField.topAnchor.constraint(equalTo: useridTextField.bottomAnchor, constant: 20),
-            passwordTextField.widthAnchor.constraint(equalToConstant: 300),
-            passwordTextField.heightAnchor.constraint(equalToConstant: 40),
-
-       
-            signupButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            signupButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
-            signupButton.widthAnchor.constraint(equalToConstant: 120),
-            signupButton.heightAnchor.constraint(equalToConstant: 40),
-
-            cancelButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            cancelButton.topAnchor.constraint(equalTo: signupButton.bottomAnchor, constant: 10),
-            cancelButton.widthAnchor.constraint(equalToConstant: 120),
-            cancelButton.heightAnchor.constraint(equalToConstant: 40)
-        ])
-    }
-
-    @objc private func signupTapped() {
-        // 유효성검사 해야 함
-    }
-
-    @objc private func cancelTapped() {
-        dismiss(animated: true, completion: nil)
-    }
+  }
+  
+  
+  
+  
+  @objc private func signupTapped() {
+    // 유효성검사 해야 함
+  }
+  
+  @objc private func cancelTapped() {
+    dismiss(animated: true, completion: nil)
+  }
 }
--- TeamOne1/View/SignUPView.swift
+++ TeamOne1/View/SignUPView.swift
@@ -6,7 +6,166 @@
 //
 
 import UIKit
+import SnapKit
 
 class SignUPView: UIView {
   
+  private let titleLabel: UILabel = {
+      let label = UILabel()
+      label.text = "회원 가입"
+      label.font = UIFont.boldSystemFont(ofSize: 24)
+      label.textAlignment = .center
+      return label
+  }()
+  
+  private let usernameTextField: UITextField = {
+      let textField = UITextField()
+      textField.placeholder = "이름"
+      textField.borderStyle = .roundedRect
+      return textField
+  }()
+  
+  private let userbirthTextField: UITextField = {
+      let textField = UITextField()
+      textField.placeholder = "생년월일"
+      textField.borderStyle = .roundedRect
+      return textField
+  }()
+  
+  private let useridTextField: UITextField = {
+      let textField = UITextField()
+      textField.placeholder = "ID"
+      textField.borderStyle = .roundedRect
+      return textField
+  }()
+
+  private let passwordTextField: UITextField = {
+      let textField = UITextField()
+      textField.placeholder = "PW"
+      textField.borderStyle = .roundedRect
+      textField.isSecureTextEntry = true
+      return textField
+  }()
+
+  
+
+  private let signupButton: UIButton = {
+      let button = UIButton(type: .system)
+      button.setTitle("Sign Up", for: .normal)
+      button.backgroundColor = .systemBlue
+      button.setTitleColor(.white, for: .normal)
+      button.layer.cornerRadius = 5
+      //button.addTarget(self, action: #selector(signupTapped), for: .touchUpInside)
+      return button
+  }()
+
+  private let cancelButton: UIButton = {
+      let button = UIButton(type: .system)
+      button.setTitle("Cancel", for: .normal)
+      //button.addTarget(self, action: #selector(cancelTapped), for: .touchUpInside)
+      return button
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.backgroundColor = .blue
+    setupLayout()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func setupLayout() {
+    
+    [
+      titleLabel,
+      usernameTextField,
+      userbirthTextField,
+      useridTextField,
+      passwordTextField,
+      signupButton,
+      cancelButton
+    ].forEach{addSubview($0)}
+    
+
+    
+    titleLabel.snp.makeConstraints{
+      $0.top.equalTo(safeAreaLayoutGuide).offset(20)
+      $0.centerX.equalTo(safeAreaLayoutGuide)
+      $0.width.equalTo(300)
+      $0.height.equalTo(40)
+    }
+
+    usernameTextField.snp.makeConstraints{
+      
+      $0.top.equalTo(titleLabel.snp.bottom).offset(50)
+      $0.centerX.equalTo(safeAreaLayoutGuide)
+      $0.width.equalTo(300)
+      $0.height.equalTo(40)
+    }
+    userbirthTextField.snp.makeConstraints{
+      $0.top.equalTo(usernameTextField.snp.bottom).offset(20)
+      $0.centerX.equalTo(safeAreaLayoutGuide)
+      $0.width.equalTo(300)
+      $0.height.equalTo(40)
+    }
+    useridTextField.snp.makeConstraints{
+      $0.top.equalTo(userbirthTextField.snp.bottom).offset(20)
+      $0.centerX.equalTo(safeAreaLayoutGuide)
+      $0.width.equalTo(300)
+      $0.height.equalTo(40)
+    }
+    passwordTextField.snp.makeConstraints{
+      $0.top.equalTo(useridTextField.snp.bottom).offset(20)
+      $0.centerX.equalTo(safeAreaLayoutGuide)
+      $0.width.equalTo(300)
+      $0.height.equalTo(40)
+    }
+    signupButton.snp.makeConstraints{
+      $0.top.equalTo(passwordTextField.snp.bottom).offset(50)
+      $0.centerX.equalTo(safeAreaLayoutGuide)
+      $0.width.equalTo(120)
+      $0.height.equalTo(40)
+    }
+    cancelButton.snp.makeConstraints{
+      $0.top.equalTo(signupButton.snp.bottom).offset(50)
+      $0.centerX.equalTo(safeAreaLayoutGuide)
+      $0.width.equalTo(120)
+      $0.height.equalTo(40)
+    }
+    
+    
+      
+      
+      
+     
+
+      
+     
+//      userbirthTextField.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 20),
+//
+//      
+//      useridTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//      useridTextField.topAnchor.constraint(equalTo: userbirthTextField.bottomAnchor, constant: 20),
+//
+//      
+//      passwordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//      passwordTextField.topAnchor.constraint(equalTo: useridTextField.bottomAnchor, constant: 20),
+//      
+//      
+//      
+//      signupButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//      signupButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+//    
+//      signupButton.widthAnchor.constraint(equalToConstant: 120),
+//      signupButton.heightAnchor.constraint(equalToConstant: 40),
+//      
+//      cancelButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//      cancelButton.topAnchor.constraint(equalTo: signupButton.bottomAnchor, constant: 10),
+//      cancelButton.widthAnchor.constraint(equalToConstant: 120),
+//      cancelButton.heightAnchor.constraint(equalToConstant: 40)
+//    ])
+//    
+  }
 }
