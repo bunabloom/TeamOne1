197751da11870cbc2ff63a5cbc2a4903a23912b8
bloom <kimrinoffdev@gmail.com>
2024-07-26 19:41:47 +0900

    123

---
+++ TeamOne1/View/MainView/MvListTableViewCell.swift
@@ -0,0 +1,87 @@
+//
+//  MvListTableViewCell.swift
+//  TeamOne1
+//
+//  Created by 유민우 on 7/23/24.
+//
+
+import UIKit
+import SnapKit
+
+final class MvListTableViewCell: UITableViewCell {
+  static let id = "MvListTableViewCell"
+  
+  weak var movieListViewController: MovieListViewController?
+  var movies: [MovieListModel] = []
+  
+  let titleLabel: UILabel = {
+    let lb = UILabel()
+    lb.font = UIFont(name: "NanumSquareNeo-dEb", size: 20)
+    return lb
+  }()
+  
+  let collectionView: UICollectionView = {
+          let layout = UICollectionViewFlowLayout()
+          layout.scrollDirection = .horizontal
+          layout.itemSize = CGSize(width: 100, height: 200) // 아이템 높이를 200으로 변경하여 제목을 포함할 수 있게 설정
+          let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+          return collectionView
+      }()
+  
+  
+  
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configureUI() {
+    contentView.addSubview(titleLabel)
+    contentView.addSubview(collectionView)
+    
+    titleLabel.snp.makeConstraints {
+      $0.top.left.right.equalToSuperview().inset(10)
+    }
+    
+    collectionView.snp.makeConstraints {
+      $0.top.equalTo(titleLabel.snp.bottom).offset(10)
+      $0.left.right.bottom.equalToSuperview().inset(10)
+      $0.height.equalTo(200) // 높이를 고정하여 스크롤 문제가 발생하지 않도록 설정
+    }
+    
+    collectionView.dataSource = self
+    collectionView.delegate = self
+    collectionView.register(MvCollectionViewCell.self, forCellWithReuseIdentifier: MvCollectionViewCell.id)
+    
+    collectionView.showsHorizontalScrollIndicator = false // 스크롤바 숨김 설정
+  }
+  
+  func updateCollectionView(with movies: [MovieListModel]) {
+    self.movies = movies
+    collectionView.reloadData()
+  }
+}
+
+extension MvListTableViewCell: UICollectionViewDataSource, UICollectionViewDelegate {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return movies.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MvCollectionViewCell.id, for: indexPath) as! MvCollectionViewCell
+    let movie = movies[indexPath.item]
+    cell.configure(with: movie)
+    return cell
+  }
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    let selectMovie = movies[indexPath.item]
+    movieListViewController?.showMovieDetail(with: selectMovie)
+    //        movieListViewController?.navigationController?.pushViewController(MovieDetailViewController(), animated: true)
+  }
+}
